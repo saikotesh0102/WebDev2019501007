307e696e01667da3a9816c3618647cb9aa16d86c
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-04-13 19:56:33 +0530

    Test Files Updated

--- Day - 6/Least Recently Used Cache/Test.py
+++ Day - 6/Least Recently Used Cache/Test.py
@@ -3,14 +3,14 @@ from Solution import LRUCache
 def main():
 	test = LRUCache(3)
 	test.put(1,"CT")
-	test.put(2,"Python")
 	test.put(3,"IDS")
-	assert test.input_data == {1:"CT", 2:"Python",3:"IDS"}
+	test.put(2,"Python")
+	assert test.input_data == {1:"CT", 3:"IDS",2:"Python"}
 	test.put(3, "Introduction to Data Science")
-	assert test.input_data == {1:"CT", 2:"Python",3:"Introduction to Data Science"}
+	assert test.input_data == {1:"CT",3:"Introduction to Data Science", 2:"Python"}
 	test.put(4, "ADS")
 	test.put(5, "Java")
-	assert test.input_data == {3:"Introduction to Data Science", 4:"ADS", 5:"Java"}
+	assert test.input_data == {2:"Python", 4:"ADS", 5:"Java"}
 	print("Assert Passed")
 
 if __name__ == "__main__":
