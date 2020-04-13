523e5e272f21234afe43c5a9720fc88031906d4c
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-04-13 22:12:05 +0530

    Methods updated

--- Day - 6/Least Recently Used Cache/Solution.py
+++ Day - 6/Least Recently Used Cache/Solution.py
@@ -1,23 +1,31 @@
-import collections
-
 class LRUCache:
 	def __init__(self, capacity):
 		super().__init__()
 		self.capacity = capacity
-		self.input_data = collections.OrderedDict()
+		self.input_data = {}
+		self.listCache = []
 	
 	def get(self, key):
 		'''
 			return value of -1
 		'''
-		if key in self.input_data.keys():
+		if key in self.input_data:
+			self.listCache.remove(key)
+			self.listCache.append(key)
 			return self.input_data[key]
+		else:
+			return -1
 
 	def get_cache(self):
-		return self.input_data
+		return self.listCache
 
 	def put(self,key,value):
-		if key not in self.input_data.keys():
-			if len(self.input_data) >= self.capacity:
-				self.input_data.popitem(last = False)
+		if len(self.listCache) == self.capacity:
+			if key in self.input_data:
+				self.listCache.remove(key)
+			else:
+				x = self.listCache.pop(0)
+				del self.input_data[x]
+		self.listCache.append(key)
 		self.input_data[key] = value
+
