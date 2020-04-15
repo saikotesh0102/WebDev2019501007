1024c6c49b21bb63f3cbb53bc42aa0f1a632d0ec
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-04-15 14:03:24 +0530

    Main Page Added

--- project1/application.py
+++ project1/application.py
@@ -1,26 +1,30 @@
 import os
 
-from flask import Flask, session
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import Flask, session, render_template
+# from flask_session import Session
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+# # Check for environment variable
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
 
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# # Configure session to use filesystem
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
-# Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
+# # Set up database
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
+@app.route("/login")
 def index():
-    return "Project 1: TODO"
+    return render_template("main.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
