ef77f284e3ef2eef98734b1dae442278fa400b44
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-04-15 16:59:13 +0530

    Registration page Added and Linked to main page

--- project1/application.py
+++ project1/application.py
@@ -25,6 +25,6 @@ app = Flask(__name__)
 def index():
     return render_template("main.html")
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
+@app.route("/register")
+def register():
+    return render_template("registration.html")
