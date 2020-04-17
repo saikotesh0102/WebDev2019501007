59fa1482b7066f29b21c761af3b37f36e9370328
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-04-17 11:15:19 +0530

    Registration Page

--- project1/application.py
+++ project1/application.py
@@ -4,23 +4,23 @@ import logging
 from flask import Flask, session, render_template, request, redirect
 from flask_session import Session
 import hashlib
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-# # Check for environment variable
-# if not os.getenv("DATABASE_URL"):
-#     raise RuntimeError("DATABASE_URL is not set")
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# # Set up database
-# engine = create_engine(os.getenv("DATABASE_URL"))
-# db = scoped_session(sessionmaker(bind=engine))
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/", methods = ['GET', 'POST'])
 @app.route("/login", methods = ['GET', 'POST'])
@@ -36,14 +36,18 @@ def register():
         session["data"] = []
         fname = request.form.get('firstname')
         lname = request.form.get('lastname')
+        name = fname + " " + lname
         email = request.form.get('email')
-        password = request.form.get('password')
-        password = hashlib.md5(password.encode()).hexdigest()
-        session["data"].append(fname)
-        session["data"].append(lname)
-        session["data"].append(email)
-        session["data"].append(password)
-        return render_template("profile.html",notesdata = session["data"])
+        pwd = request.form.get('password')
+        password = hashlib.md5(pwd.encode()).hexdigest()
+        repwd = request.form.get('repassword')
+        repassword = hashlib.md5(repwd.encode()).hexdigest()
+        if password == repassword:
+            session["data"].append(name)
+            session["data"].append(email)
+            return render_template("profile.html",fullname = session["data"][0])
+        else:
+            return render_template("registration.html")
 
 @app.route("/profile", methods = ['GET', 'POST'])
 def profile():
