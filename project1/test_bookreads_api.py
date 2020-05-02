58a730adffdaba3e45574d46aa6baaafb603f3ba
chaithvika <kittulkc6697@msitprogram.net>
2020-05-02 17:40:22 +0530

    testing and week_4 done

--- project1/test_bookreads_api.py
+++ project1/test_bookreads_api.py
@@ -1,10 +1,34 @@
-from application import *
-from models import *
+import os,unittest
+from models import Book,app,db
+from flask import session
 
-def test_bookreads_api():
-	book = Book.query.get("1416949658")
-	# response = bookreads_api("1416949658")
-	# print(response)
-	print(book.title)
+class TestCase(unittest.TestCase):
+    def setUp(self):
+		app.config['TESTING'] = True
+		app.config['WTF_CSRF_ENABLED'] = False
+		app.config['DEBUG'] = False
 
-test_bookreads_api()
+		if not os.getenv("DATABASE_URL"):
+			raise RuntimeError("DATABASE_URL is not set")
+		self.app = app.test_client()
+		self.assertEqual(app.DEBUG, False)
+		db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_bookreads_api(self):
+        book = Book(isbn = "123456", name = "2States", author = "Chetan Bhagat", publicationyear = "2015")
+        db.session.add(book)
+        db.session.commit()
+        name = Book.query.get('123456').name
+        assert name == '2States'
+        book = Book(isbn = "123457", name = "Revolution2020", author = "Chetan Bhagat", publicationyear = "2013")
+        db.session.add(book)
+        db.session.commit()
+        name = Book.query.get('Chetan Bhagat').name
+        assert name == ['2States','Revolution2020']
+
+if __name__ == '__main__':
+    unittest.main()
